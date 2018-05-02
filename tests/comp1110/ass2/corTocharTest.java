5fe3365fce89bc915732ab6160589b3eb048bed4
Rufus Raja <u6275198@anu.edu.au>
2018-05-02 16:54:59 +1000

    updated comments to the tests

--- tests/comp1110/ass2/corTocharTest.java
+++ tests/comp1110/ass2/corTocharTest.java
@@ -9,6 +9,7 @@ import static junit.framework.TestCase.assertEquals;
  * Created by Rufus Raja (u6275198) to test the corTochar() function.
  */
 public class corTocharTest extends WarringStatesGame{
+    //Declare an array representing the game board and the positions
     char testcors[][] =
             {
                     {'4','Y','S','M','G','A'},
@@ -19,6 +20,7 @@ public class corTocharTest extends WarringStatesGame{
                     {'9','3','X','R','L','F'}
             };
 
+    //Test for a single value to check if the character returned matches the coordinate position
     @Test
     public void TestOne()
     {
@@ -26,6 +28,7 @@ public class corTocharTest extends WarringStatesGame{
        assertEquals("The expected result is "+testcors[1][0]+" but got "+c,c,testcors[1][0]);
     }
 
+    //Test for two values to check if the character returned matches the coordinate position
     @Test
     public void TestTwo()
     {
@@ -35,10 +38,11 @@ public class corTocharTest extends WarringStatesGame{
         assertEquals("The expected result is "+testcors[4][5]+" but got "+c1,c1,testcors[4][5]);
     }
 
-
+    //Test for all possible values to check if the character returned matches the coordinate position
     @Test
     public void TestComplete()
     {
+        //running a loop within the 6*6 range to pick up all the possible coordinates within the board to be tested.
         for(int x=0;x<6;x++)
         {
             for(int y=0;y<6;y++)
