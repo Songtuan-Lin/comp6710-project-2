f9615e808f9797554519a1cc29ab920174956ffa
xujiajia <u6528982@anu.edu.au>
2018-05-17 17:24:06 +1000

    stage F

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -332,8 +332,8 @@ public class Game extends Application {
         {
             button.setDisable(true);
             textField.setDisable(true);
-      //      getMove = alpha_beta_search(placement1, boardMatrix) + "";//BotMove()+"";
-            getMove = BotMove()+"";
+            getMove = alpha_beta_search(placement1, boardMatrix) + "";//BotMove()+"";
+            //getMove = BotMove()+"";
             delay(1000, new Runnable(){ public void run(){ nextStep();} });
             //nextStep();
         }
